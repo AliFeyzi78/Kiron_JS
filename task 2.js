540a539a1f6e7fa9f46aa6be364e84c137870036
AliFeyzi78 <zeron1g2k@gmail.com>
2021-03-19 01:52:07 +0200

    finished task-2

--- task 2.js
+++ task 2.js
@@ -3,6 +3,13 @@
 // Copyright (C) Coursera 2020
 
 // classic declaration
+// function doubleMe(x) {
+//   return x * 2;
+// }
+
+var doubleMe = function (x) {
+  return x * 2;
+};
 
 // as an expression, assign to variable
 
@@ -10,8 +17,11 @@ log("pi * 2 = " + doubleMe(3.14));
 
 // setTimeout, expecting a function for callback
 
-  // we will write the call back function that does this when time's up
-  // log("4 seconds timer stopped: " + Date().toLocaleString());
+// we will write the call back function that does this when time's up
+// log("4 seconds timer stopped: " + Date().toLocaleString());
+var timesUpFunction = function () {
+  log("4 seconds timer stopped: " + Date().toLocaleString());
+};
 
 function startTimerClicked() {
   log("4 seconds timer started: " + Date().toLocaleString());
@@ -24,19 +34,18 @@ var isTicking = false;
 var timerID;
 
 function tickerClicked() {
-
   if (isTicking) {
     isTicking = false;
     clearInterval(timerID);
     document.getElementById("ticker-button").value = "Start Ticker!";
     log("Ticker stopped!");
-  }
-  else {
+  } else {
     isTicking = true;
     // write the function code to log "tick ..." at each interval
-    timerID = setInterval( { /* insert anonymous function here */ }, 1000);
+    timerID = setInterval(function () {
+      log("tick ...");
+    }, 1000);
     document.getElementById("ticker-button").value = "Stop Ticker!";
     log("Ticker started!");
   }
-
 }
